1971dda3291f88492ac8da0fdd850ace4f5431b6
Afe T <otejiri@protonmail.com>
2022-08-26 16:28:38 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const { getProfitability } = require("../controllers/btcInfo");
 
 const router = express.Router();
 
-router.get("/", getProfitability);
+router.get("/api/", getProfitability);
 
 module.exports = router;
